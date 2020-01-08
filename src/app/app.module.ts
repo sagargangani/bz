85fa54cd8f6ddecde7f0b55db3358fa6f242a1bd
sagargangani <ganganisagar33@gmail.com>
2020-01-08 10:49:23 +0530

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ColorComponent } from './color/color.component';
+import { UserComponent } from './user/user.component';
+import { CountryComponent } from './country/country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ColorComponent,
+    UserComponent,
+    CountryComponent
   ],
   imports: [
     BrowserModule,
